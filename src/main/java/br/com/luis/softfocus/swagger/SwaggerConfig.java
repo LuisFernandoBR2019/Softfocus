9d4aaaf9e0ff4911397dfd3d81e9c092ad1ff79f
Luis Fernando <nando_srs@hotmail.com>
2022-05-09 18:08:40 -0300

    Ajuste pacote correto do controller da API no Swagger

--- src/main/java/br/com/luis/softfocus/swagger/SwaggerConfig.java
+++ src/main/java/br/com/luis/softfocus/swagger/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.luis.softfocus.controller"))
+				.apis(RequestHandlerSelectors.basePackage("br.com.luis.softfocus.controllerapi"))
 				.paths(PathSelectors.any()).build().apiInfo(getAppInfo()).enable(swaggerEnabled);
 	}
 
